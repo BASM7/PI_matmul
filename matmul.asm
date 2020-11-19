a8c07a8cb6f19014503057f6562a9377bcef265f
BASM7 <j.alonsomm07@gmail.com>
2020-11-18 19:06:20 -0600

    almost there

--- matmul.asm
+++ matmul.asm
@@ -1,29 +1,89 @@
 bits 64
 
 section .text
-    global _sumRow
+    global _copy_row
+    global _quickMatrixMul
+    global _test
     global _WinMain@16
 
 _WinMain@16:
 
-; 1º: RCX
-; 2º: RDX
-; 3º: R8
-; 4º: R9
-; >4: Stack.
+_test:
+    ;mov     r10, rcx
+    xor     rax, rax
 
-_sumRow:
-    movlps  xmm1, [ecx+0*4]
-    movlps  xmm0, [ecx+1*4]
+    add     rax, rcx
+    add     rax, rdx
+    add     rax, r8
+    add     rax, r9
+    add     rax, [rsp+40]
+    add     rax, [rsp+48]
 
-    addss   xmm0, xmm1
     ret     16
 
-    ; xmml -> 128 
 
+_quickMatrixMul:
 
-    ; ._loop:    
-    ; addss   xmm0, xmm0
-    ; dec     rdx
-    ; cmp     rdx, 0
-    ; jne     ._loop
+    xor         rax, rax
+    xor         rbx, rbx
+
+    mov         r11, [rsp+40]       ; get m from the stack.
+    mov         r12, [rsp+48]       ; get p from the stack.
+
+    mov         rax, 0              ; counter for the first matrix.
+    mov         r10, 0              ; counter for the third matrix.
+    ._loop:
+    
+    mov         rbx, 0              ; counter for the second matrix.
+    ._loop_m2:
+    xorps       xmm1, xmm1
+    movups      xmm1, [rcx+rax*4]
+    xorps       xmm2, xmm2
+    movaps      xmm2, [rdx+rbx*4]
+
+    call        _dot_product
+    ;movss       [r8+r10*4], xmm0   ; this saves the value on the third matrix.  
+
+    ;inc         r10   
+    add         rbx, 4              ; add 4 to get the next column.
+    add         rax, 4
+    cmp         rbx, r11            ; stop condition is 4*columns.
+    jne         ._loop_m2
+
+    movss       [r8+r10*4], xmm0   ; this saves the value on the third matrix.  
+
+    ;add         rax, 4
+    cmp         rax, r9           ; stop condition is 4*rows.
+    ;jne         ._loop
+    ret         16
+
+_dot_product:
+    ;   returns the dot product of two vectors.
+    ;   expects vectors on xmm1, xmm2.
+    ;   leaves the dot product on xmm0.
+    xorps       xmm4, xmm4
+
+    movups      xmm3, xmm1
+    movups      xmm4, xmm0
+
+    xorps       xmm0, xmm0
+
+    mulps       xmm1, xmm2  
+    haddps      xmm0, xmm1
+    movhlps     xmm0, xmm0
+    haddps      xmm0, xmm0
+
+    addss       xmm0, xmm4
+
+    movups      xmm1, xmm3
+    ret         
+
+_copy_row:
+    mov         rax, 0
+    ._loop:
+    movups      xmm1, [rcx+rax*4]
+    movups      [rdx+rax*4], xmm1
+    add         rax, 4
+    cmp         rax, 16
+    jne         ._loop
+    ret         16
--- matmul.cpp
+++ matmul.cpp
@@ -5,18 +5,12 @@
 #include <iomanip>
 #include <conio.h>
 
-/*
-void quickMatrixMul(float* matrixNM, float* matrixMP, float* matrixNP, unsigned n, unsigned m, unsigned p){
-
-}
-*/
+void quickMatrixMul(float* matrixNM, float* matrixMP, float* matrixNP, unsigned int n, unsigned int m);
 
 float* loadMatrix(const char* filename, unsigned int &rows, unsigned int &cols ){
     std::ifstream matrix_file;
     matrix_file.open(filename);
-    std::string data;
-
-    float* new_matrix;
+    std::string data;   
 
     rows, cols = 0;
 
@@ -31,6 +25,8 @@ float* loadMatrix(const char* filename, unsigned int &rows, unsigned int &cols )
         }
     }       
     
+    float* new_matrix;
+
     // read the rest of the matrix.
     if (rows > 0 && cols > 0){
         new_matrix = new float[rows*cols];
@@ -61,38 +57,53 @@ bool compare(float* matrixA, float* matrixB, unsigned int elements){
     return are_equal;
 }
 
-void printMatrix_test(float* matrix, unsigned int rows, unsigned int cols){
-    for (int i = 0; i < rows; ++i){
-        for (int j = 0; j < cols; ++j){
+void printMatrix(float* matrix, unsigned int rows, unsigned int cols){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
             std::cout << std::setw(5) << matrix[i*cols+j] << " ";          
         }
         std::cout << std::endl;
     }
 }
 
-extern "C" int _someFunc();
-extern "C" float _sumRow(float* matrix, unsigned cols);
+void transpose(float* matrix, unsigned int &rows, unsigned int &cols){
+    for (int i = 0; i < rows; i++){
+        for (int j = i + 1; j < cols; j++){
+            float temp = matrix[i*cols+j];
+            matrix[i*cols+j] = matrix[j*cols+i];
+            matrix[j*cols+i] = temp;
+        }
+    }
+    int temp = rows;
+    rows = cols;
+    cols = temp;
+}
+
+extern "C" int _test(int x,int y,int z, int  a,int b, int c);
+
+extern "C" float _quickMatrixMul(float* matrix1, float* matrix2, float* matrix_output, unsigned int n, unsigned int m, unsigned int p);
+extern "C" float _copy_row(float* matrix1, float* matrix_output);
 
 int main(){
+    //const char* matrix2_file = "Casos_prueba/test.txt";
 
-    const char* matrix1_file = "Casos_prueba/test.txt";
+    // const char* matrix1_file = "Casos_prueba/case0_matrix1.txt";
+    // const char* matrix2_file = "Casos_prueba/case0_matrix2.txt";
+    // const char* output_file = "Casos_prueba/case0_output.txt";
 
-    //const char* matrix1_file = "Casos_prueba/case0_matrix1.txt";
-    const char* matrix2_file = "Casos_prueba/case0_matrix2.txt";
-    const char* output_file = "Casos_prueba/case0_output.txt";
-
-    // const char* matrix1_file = "Casos_prueba/case1_matrix1.txt";
+    //const char* matrix1_file = "Casos_prueba/case1_matrix1.txt";
     // const char* matrix2_file = "Casos_prueba/case1_matrix2.txt";
     // const char* output_file = "Casos_prueba/case1_output.txt";
 
-    // const char* matrix1_file = "Casos_prueba/case2_matrix1.txt";
-    // const char* matrix2_file = "Casos_prueba/case2_matrix2.txt";
-    // const char* output_file = "Casos_prueba/case2_output.txt";
+    const char* matrix1_file = "Casos_prueba/case2_matrix1.txt";
+    const char* matrix2_file = "Casos_prueba/case2_matrix2.txt";
+    const char* output_file = "Casos_prueba/case2_output.txt";
 
     float* matrix1;
     float* matrix2;
-    float* matrixO;
-
+    float* matrixO_expected;
+    float* matrixO_result;
+    
     unsigned int rows_matrix1;
     unsigned int cols_matrix1;
     unsigned int rows_matrix2;
@@ -100,21 +111,35 @@ int main(){
     unsigned int rows_matrixO;
     unsigned int cols_matrixO;
 
+    matrixO_result = new float[16];
+
     matrix1 = loadMatrix(matrix1_file, rows_matrix1, cols_matrix1);
     matrix2 = loadMatrix(matrix2_file, rows_matrix2, cols_matrix2);
-    matrixO = loadMatrix(output_file, rows_matrixO, cols_matrixO);
+    matrixO_expected = loadMatrix(output_file, rows_matrixO, cols_matrixO);
 
     std::cout << "Se cargaron las matrices :" << std::endl;
-    printMatrix_test(matrix1, rows_matrix1, cols_matrix1);
+    printMatrix(matrix1, rows_matrix1, cols_matrix1);
     std::cout << std::endl;
-    // printMatrix_test(matrix2, rows_matrix2, cols_matrix2);
-    // std::cout << std::endl;
+    printMatrix(matrix2, rows_matrix2, cols_matrix2);
+    std::cout << std::endl;
 
     //std::cout << compare(matrix1, matrix1, rows_matrix1*cols_matrix1) << std::endl;
 
+    //float* matrix_test = new float[16];
     
+    //std::cout << _test(1, 2, 3, 4, 5, 6) << std::endl;
 
-    std::cout << _sumRow(matrix1, cols_matrix1) << std::endl;
+    transpose(matrix2, rows_matrix2, cols_matrix2);
+    _quickMatrixMul(matrix1, matrix2, matrixO_result, rows_matrix1*4, 4*4, cols_matrixO*4);
+
+
+    // //std::cout << _quickMatrixMul(matrix1, matrix2, matrixO_result) << std::endl;
+    printMatrix(matrixO_result, rows_matrixO, cols_matrixO);
+    //std::cout << "Transpose: " << std::endl;
+    //transpose(matrix1, rows_matrix1, cols_matrix1);
+    //printMatrix(matrix1, cols_matrix1, rows_matrix1);
+    //_copy_row(matrix1, matrix_test);
+    //printMatrix_test(matrix_test, 4, 4);
 
     //_getch();
 
@@ -122,7 +147,4 @@ int main(){
 }
 
 // Compilation.
-// g++ -g -no-pie -o matmul matmul_n.o
-// g++ -c -g -no-pie -o matmul_c.o matmul.cpp && g++ -g -no-pie -o matmul matmul_c.o
-
 // nasm -g -f elf64 -o matmul_n.o matmul.asm && g++ -c -g -no-pie -o matmul_c.o matmul.cpp && g++ -g -no-pie -o matmul matmul_n.o matmul_c.o
